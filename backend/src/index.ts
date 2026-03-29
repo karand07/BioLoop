2cf441538e985faa1d651f5951e70b024e65a6aa
karand07 <karandanole07@gmail.com>
2026-03-29 14:09:22 +0530

    logistics features

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,13 @@ import { port } from "./envConfig.js";
 import { userRoute } from "./modules/user/user.routes.js";
 import { farmerRoute } from "./modules/farmer/farmer.routes.js";
 import { companyRoute } from "./modules/company/company.routes.js";
+import { logisticsRoute } from "./modules/logistics/logistics.routes.js";
 const app = express();
 app.use(express.json());
 
 app.use("/user", userRoute);
 app.use("/farmer", farmerRoute);
 app.use("/company", companyRoute);
+app.use("/logistics", logisticsRoute);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
