c39064e07195ac17bfb0be406e46bd703c1c2f9a
karand07 <karandanole07@gmail.com>
2026-04-12 12:39:08 +0530

    formating order_request

--- backend/src/modules/order_request/orderRequest.schemas.ts
+++ backend/src/modules/order_request/orderRequest.schemas.ts
@@ -2,13 +2,13 @@ import z from "zod";
 import { RequestStatus } from "../../generated/prisma/enums.js";
 
 export const requestShema = z.object({
-    listing_id:z.number("request_id required"),
-    requested_quantity:z.number("quantity must be a number").nonnegative(),
-    offered_price:z.number("offered price must be a number").nonnegative(),
-    message:z.string().optional(),
-    status:z.enum(RequestStatus).default("pending"),
-    is_active:z.boolean().default(true)
-})
+  listing_id: z.number("request_id required"),
+  requested_quantity: z.number("quantity must be a number").nonnegative(),
+  offered_price: z.number("offered price must be a number").nonnegative(),
+  message: z.string().optional(),
+  status: z.enum(RequestStatus).default("pending"),
+  is_active: z.boolean().default(true),
+});
 
 export type requestTypes = z.infer<typeof requestShema>;
 
--- backend/src/modules/order_request/orderReuest.routes.ts
+++ backend/src/modules/order_request/orderReuest.routes.ts
@@ -1,19 +1,52 @@
 import { Router } from "express";
-import { authenticate, isCompany, isFarmer } from "../../middleware/auth.middleware.js";
+import {
+  authenticate,
+  isCompany,
+  isFarmer,
+} from "../../middleware/auth.middleware.js";
 import { requestController } from "./orderRequest.controller.js";
 const requestRoute = Router();
 
 // company routes
-requestRoute.post("/", authenticate,  isCompany, requestController.create);
-requestRoute.get("/my", authenticate,  isCompany, requestController.getMySentRequests);
-requestRoute.delete("/:request_id", authenticate,  isCompany, requestController.cancelRequest);
+requestRoute.post("/", authenticate, isCompany, requestController.create);
+requestRoute.get(
+  "/my",
+  authenticate,
+  isCompany,
+  requestController.getMySentRequests,
+);
+requestRoute.delete(
+  "/:request_id",
+  authenticate,
+  isCompany,
+  requestController.cancelRequest,
+);
 
 // farmer routes
-requestRoute.get("/incoming", authenticate, isFarmer, requestController.getIncomingRequests);
-requestRoute.patch("/:request_id/accept", authenticate,  isFarmer, requestController.acceptRequest);
-requestRoute.patch("/:request_id/reject", authenticate,  isFarmer, requestController.rejectRequest);
+requestRoute.get(
+  "/incoming",
+  authenticate,
+  isFarmer,
+  requestController.getIncomingRequests,
+);
+requestRoute.patch(
+  "/:request_id/accept",
+  authenticate,
+  isFarmer,
+  requestController.acceptRequest,
+);
+requestRoute.patch(
+  "/:request_id/reject",
+  authenticate,
+  isFarmer,
+  requestController.rejectRequest,
+);
 
 // shared routes
-requestRoute.get("/:request_id", authenticate,  requestController.getRequestById);
+requestRoute.get(
+  "/:request_id",
+  authenticate,
+  requestController.getRequestById,
+);
 
-export{requestRoute};
+export { requestRoute };
